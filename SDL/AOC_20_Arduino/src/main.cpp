6aa6559dd6c91e3b2eb4020a3e10c0b6489379dd
TheMathWiz56 <146789934+TheMathWiz56@users.noreply.github.com>
2026-04-08 14:25:00 -0500

    Testing code for 1 servo motor

--- SDL/AOC_20_Arduino/src/main.cpp
+++ SDL/AOC_20_Arduino/src/main.cpp
@@ -1,18 +1,35 @@
 #include <Arduino.h>
+#include <DRV8834.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define RACK_STEPS 200
+#define RACK_DIR_PIN 8
+#define RACK_STEP_PIN 9
+#define RACK_RPM 60
+
+#define RACK_LEFT_BTN 3
+#define RACK_RIGHT_BTN 2
+
+// Board is 1/4 microstepping by default
+#define MICROSTEPS 4
+
+DRV8834 rackMotor(RACK_STEPS, RACK_DIR_PIN, RACK_STEP_PIN);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  rackMotor.begin(RACK_RPM, MICROSTEPS); 
+  //rackMotor.setSpeedProfile(BasicStepperDriver::LINEAR_SPEED, 1000, 1000); 
+
+  pinMode(RACK_LEFT_BTN, INPUT);
+  pinMode(RACK_RIGHT_BTN, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (digitalRead(RACK_LEFT_BTN) == HIGH) {
+    Serial.println("Left button pressed");
+    rackMotor.move(-800); // Move left by 800 microsteps
+  }
+  if (digitalRead(RACK_RIGHT_BTN) == HIGH) {
+    Serial.println("Right button pressed");
+    rackMotor.move(800); // Move right by 800 microsteps
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
