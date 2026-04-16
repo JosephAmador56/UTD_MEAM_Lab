91576153abe0106974904ec1f87d4296abc6caae
TheMathWiz56 <amadorjosephg@gmail.com>
2026-04-16 17:57:52 -0500

    changed to keyboard control

--- SDL/AOC_20_Arduino/src/main.cpp
+++ SDL/AOC_20_Arduino/src/main.cpp
@@ -6,30 +6,54 @@
 #define RACK_STEP_PIN 9
 #define RACK_RPM 60
 
-#define RACK_LEFT_BTN 3
-#define RACK_RIGHT_BTN 2
-
 // Board is 1/4 microstepping by default
 #define MICROSTEPS 4
 
 DRV8834 rackMotor(RACK_STEPS, RACK_DIR_PIN, RACK_STEP_PIN);
 
+#define LED_PIN 13
+
 void setup() {
+  Serial.begin(9600);
   rackMotor.begin(RACK_RPM, MICROSTEPS); 
   //rackMotor.setSpeedProfile(BasicStepperDriver::LINEAR_SPEED, 1000, 1000); 
 
-  pinMode(RACK_LEFT_BTN, INPUT);
-  pinMode(RACK_RIGHT_BTN, INPUT);
+  //pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  if (digitalRead(RACK_LEFT_BTN) == HIGH) {
-    Serial.println("Left button pressed");
-    rackMotor.move(-800); // Move left by 800 microsteps
-  }
-  else if (digitalRead(RACK_RIGHT_BTN) == HIGH) {
-    Serial.println("Right button pressed");
-    rackMotor.move(800); // Move right by 800 microsteps
+  Serial.println("Please selected an option:");
+  Serial.println("1. Move left");
+  Serial.println("2. Move right");
+  Serial.println("3. Move x microsteps");
+
+  int choice = waitnReadInt();
+
+  switch (choice) {
+    case 1:
+      Serial.println("Moving left...");
+      rackMotor.move(-800); // Move left by 800 microsteps
+      break;
+    case 2:
+      Serial.println("Moving right...");
+      rackMotor.move(800); // Move right by 800 microsteps
+      break;
+    case 3:
+      Serial.println("Enter number of microsteps to move (positive for right, negative for left):");
+      int steps = waitnReadInt();
+      Serial.print("Moving ");
+      Serial.print(steps);
+      Serial.println(" microsteps...");
+      rackMotor.move(steps);
+      break;
+    default:
+      Serial.println("Invalid option. Please try again.");
   }
 }
 
+int waitnReadInt() {
+  while (Serial.available() == 0) {
+    // wait for input
+  }
+  return Serial.parseInt();
+}
